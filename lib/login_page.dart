452a18f1cd7461a1e4f3845f4e9ad656e7f118da
Aadit Antony <aaditantony1112@gmail.com>
2025-07-11 02:10:44 +0530

    short change in the ui

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,48 +14,52 @@ class LoginPage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.lock, size: 200),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                final email = emailController.text;
-                final password = passwordController.text;
-                ref
-                    .read(loginViewModelProvider.notifier)
-                    .login(email, password);
-              },
-              child: Text("Login"),
-            ),
-            SizedBox(height: 20),
-            loginState.when(
-              data: (loginModel) {
-                if (loginModel.token.isNotEmpty) {
-                  // Navigate to product list
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    Navigator.pushReplacementNamed(context, '/productList');
-                  });
-                }
-                return Container();
-              },
-              loading: () => CircularProgressIndicator(),
-              error: (err, _) =>
-                  Text(err.toString(), style: TextStyle(color: Colors.red)),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 40),
+              Icon(Icons.lock, size: 200),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 30),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  final email = emailController.text;
+                  final password = passwordController.text;
+                  ref
+                      .read(loginViewModelProvider.notifier)
+                      .login(email, password);
+                },
+                child: Text("Login"),
+              ),
+              SizedBox(height: 20),
+              loginState.when(
+                data: (loginModel) {
+                  if (loginModel.token.isNotEmpty) {
+                    // Navigate to product list
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      Navigator.pushReplacementNamed(context, '/productList');
+                    });
+                  }
+                  return Container();
+                },
+                loading: () => CircularProgressIndicator(),
+                error: (err, _) =>
+                    Text(err.toString(), style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          ),
         ),
       ),
     );
